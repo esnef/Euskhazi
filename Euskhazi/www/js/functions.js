80954024ee92681f804ed3691446e95d4c0c0139
Iker Irazu <iker@arima.eu>
2015-11-16 17:15:27 +0100

    Integrado Atarikoa

--- Euskhazi/www/js/functions.js
+++ Euskhazi/www/js/functions.js
@@ -6,7 +6,7 @@ function login() {
 	newUser.name=loginVal.toString().trim();
 	if(loginVal!=null && loginVal.toString().trim().length != 0){
 		if(checkUser(newUser)){
-			saveNewUser(newUser)
+			saveNewUser(newUser);
 			refreshUsersList();
 			loadUser(newUser.name);
 		}else{
@@ -221,4 +221,73 @@ function advice(pI,qI) {
 		}
 	}
 		
+}
+
+//ATARIKOA INIT
+function loadDataJSONAtarikoa(level){
+//	$.getJSON(fileFolder+level+'/'+fileName, function(json) {
+	$.getJSON('exams/'+level+'/Atarikoa.json', function(json) {
+	    console.log(json); // this will show the info it in firebug console
+	    examsAtarikoa=json;
+//	    examsAtarikoa[0].statements[0].statement;//lehenengo galdera iteko
+//	    examsAtarikoa[0].statements[0].answers[0];//lehenengo galderaren lehenengo erantzuna
+	});	
+}
+
+function loadAtarikoaMenu(level){
+	loadDataJSONAtarikoa(level);
+	setTimeout(function() {
+		var $menuAtarikoa = pageAtarikoa.createMenu(examsAtarikoa, level);
+		$("body").append($menuAtarikoa);
+		$("body").enhanceWithin();
+		$(':mobile-pagecontainer').pagecontainer('change', '#page_menu_atarikoa');
+	}, 50);
+	
+}
+
+function loadExamAtarikoa(i,level){
+	var $examenAtarikoa = pageAtarikoa.createExam(i, examsAtarikoa, level)
+	$("body").append($examenAtarikoa);
+	$("body").enhanceWithin();
+	$(':mobile-pagecontainer').pagecontainer('change', '#page-exam-atarikoa-'+i);
+}
+
+function checkAtarikoa(i){
+	var respondidos=0;
+	var correctos=0;
+	for(var con=0;con < examsAtarikoa[i].statements.length;con++){
+		var answer=$("input[name='radio-choice-atarikoa-"+con+"']:checked").val();
+		if(answer){
+			$("label[id|='label-radio-choice-"+con+"']").each(
+				function(index) {
+					if(index!=examsAtarikoa[i].statements[con].solution) {
+						$(this).css("color","red");
+					}
+					else
+						$(this).css({"color":"white","background-color":"green","font-size":"24px"});
+				}
+			);
+			respondidos++;
+			if(answer == examsAtarikoa[i].statements[con].solution){
+				correctos++;
+			}
+		}
+	}
+	$('#correct_answer_atarikoa').text('Erantzun zuzenak: '+correctos);
+	if(respondidos < examsAtarikoa[i].statements.length){
+		alert((examsAtarikoa[i].statements.length-respondidos)+' galdera falta zaizkizu erantzuteko');
+		return;
+	}
+	var minimoParaAprobar=examsAtarikoa[i].statements.length*(3/4);
+	
+	$buttons=$('#form-atarikoa-buttons').empty();
+	backButton='<a id="salir-atarikoa-button" href="#page_menu_atarikoa" class="ui-btn">Irten</a>';
+	$buttons.append(backButton);
+	
+	if(correctos<minimoParaAprobar){
+		alert('Ez duzu gainditu!');
+		
+	}else{
+		alert('ZORIONAK!! Gainditu duzu!');
+	}
 }
--- Euskhazi/www/js/objects.js
+++ Euskhazi/www/js/objects.js
@@ -840,9 +840,9 @@ var pageExams={
 			//Debemos crear los niveles
 			
 			var statementsDivs='<ul data-role="listview" data-inset="true">';
-			
+			alert(level);
 				statementsDivs=statementsDivs+
-				  '<li><a href="#" id="levels_li_menu_tests_level_'+level+'-proof-'+'atarikoa'+'">'+'Atarikoa'+'</a></li>';
+				  '<li><a href="#" onClick="loadAtarikoaMenu(&#39;'+level+'&#39;)" id="levels_li_menu_tests_level_'+level+'-proof-'+'atarikoa'+'">'+'Atarikoa'+'</a></li>';
 				statementsDivs=statementsDivs+
 				  '<li><a href="#" id="levels_li_menu_tests_level_'+level+'-proof-'+'idatzizkoa'+'">'+'Idatzizkoa'+'</a></li>';
 				statementsDivs=statementsDivs+
@@ -882,5 +882,100 @@ var pageExams={
 			
 };
 //MENU EXAMS END
-
+//ATARIKOA OBJECT
+var pageAtarikoa={
+		
+		createMenu: function(exams,level){
+			if(level==null || exams==null){
+				return null;
+			}
+			$('#page_menu_atarikoa').remove();
+			var pageDiv=$('<div data-role="page" id="page_menu_atarikoa" name="page_menu_atarikoa"></div>');
+			var headerDiv=
+				'<div data-role="header" data-position="fixed" >'+
+					'<h1 style="margin-left:0;margin-right:0;white-space: nowrap;overflow: visible;">'+'Atarikoa '+level+'</h1>'+
+				'</div>';
+			
+			var statementsDivs='<ul data-role="listview">';
+			
+			for(var con=0;con<exams.length;con++){
+				statementsDivs=statementsDivs+
+				  '<li><a href="#" onClick="loadExamAtarikoa(&#39;'+con+'&#39;,&#39;'+level+'&#39;)" id="menu_exam_atarikoa-'+level+'-'+con+'">'+'Azterketa '+(con+1)+'</a></li>';
+			}
+			
+			statementsDivs=statementsDivs+'</ul>';
+			
+			var contentDiv=
+				'<div data-role="content">'+statementsDivs+'</div>';
+			var footerDiv=
+				'<div data-role="footer" data-position="fixed" style="padding-top:1%;">'+
+					'<div class="ui-grid-b">'+
+						'<div class="ui-block-a" style="text-align:left;width:20%;">'+
+						'</div>'+
+						'<div class="ui-block-b" style="text-align:center;width:60%;"><h4 style="margin-bottom:1%;"></h4></div>'+
+						'<div class="ui-block-c" style="text-align:right;width:20%;">'+
+						'</div>'+
+					'</div>'+
+				'</div>';
+			pageDiv.append(headerDiv,contentDiv,footerDiv);
+			return pageDiv;
+		},
+		
+		createExam: function(i,exams,level){
+			if(exams==null || level==null){
+				alert("No se ha podido presentar en pantalla porque se ha pasado un valor menos a uno");
+				return null;
+			}else if(i>=exams.length){
+				alert("No se puede solicitar un numero de examen superior al que se tiene");
+				return null;
+			}
+			$('#page-exam-atarikoa-'+i).remove();
+			var pageDiv=$('<div id="page-exam-atarikoa-'+i+'" data-role="page" ></div>');
+			var headerDiv=
+				'<div data-role="header" data-position="fixed" >'+
+					'<h1 style="margin-left:0;margin-right:0;white-space: nowrap;overflow: visible;">'+'Atarikoa '+level+' '+(i+1)+'</h1>'+
+				'</div>';
+			
+			var statementsDivs="";
+			for(var con=0;con<exams[i].statements.length;con++){
+				statementsDivs+=
+				'<fieldset data-role="controlgroup" data-iconpos="right">'+
+				'<legend id="question-'+con+'">'+(con+1)+'.- '+exams[i].statements[con].statement+'</legend>'+
+				'<input name="radio-choice-atarikoa-'+con+'" id="radio-choice-'+con+'a" data-mini="true" value="0" type="radio"/>'+
+				'<label for="radio-choice-'+con+'a" id="label-radio-choice-'+con+'-0">'+exams[i].statements[con].answers[0].first+'</label>'+
+				'<input name="radio-choice-atarikoa-'+con+'" id="radio-choice-'+con+'b" data-mini="true" value="1" type="radio"/>'+
+				'<label for="radio-choice-'+con+'b" id="label-radio-choice-'+con+'-1">'+exams[i].statements[con].answers[0].second+'</label>'+
+				'<input name="radio-choice-atarikoa-'+con+'" id="radio-choice-'+con+'c" data-mini="true" value="2" type="radio"/>'+
+				'<label for="radio-choice-'+con+'c" id="label-radio-choice-'+con+'-2">'+exams[i].statements[con].answers[0].third+'</label>'+
+				'<input name="radio-choice-atarikoa-'+con+'" id="radio-choice-'+con+'d" data-mini="true" value="3" type="radio"/>'+
+				'<label for="radio-choice-'+con+'d" id="label-radio-choice-'+con+'-3">'+exams[i].statements[con].answers[0].fourth+'</label>'+
+				'</fieldset>';
+			}
+			statementsDivs=statementsDivs+'<div id="form-atarikoa-buttons">';
+			statementsDivs=statementsDivs+'<a id="correctExams_atarikoa" name="onClickCorrectExams" href="" onClick="checkAtarikoa(&#39;'+i+'&#39;)" class="ui-btn">Zuzendu</a>'
+			statementsDivs=statementsDivs+'<a style="display:none;" id="salir-atarikoa-button" href="#page_menu_atarikoa" class="ui-btn">Irten</a>'
+			statementsDivs=statementsDivs+'</div>';
+			statementsDivs='<form id="form-atarikoa">'+statementsDivs+'</form>';
+			
+			var contentDiv=
+				'<div data-role="content">'+statementsDivs+'</div>';
+			var footerDiv=
+				'<div data-role="footer" data-position="fixed" style="padding-top:1%;">'+
+					'<div class="ui-grid-b">'+
+						'<div class="ui-block-a" style="text-align:left;align-content:center;width:20%;">'+
+						'</div>'+
+						'<div class="ui-block-b" style="text-align:center;align-content:center;width:60%;"><h4 id="correct_answer_atarikoa" style="margin-bottom:1%;">Erantzun zuzena: 0</h4></div>'+
+						'<div class="ui-block-c" style="text-align:right;align-content:center;width:20%;">'+
+						'</div>'+
+					'</div>'+
+				'</div>';
+			pageDiv.append(headerDiv,contentDiv,footerDiv);
+			return pageDiv;
+		},
+		load: function(i) {
+			
+			alert("load1");
+		}
+			
+};
 

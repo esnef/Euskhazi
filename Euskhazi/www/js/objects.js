8cc6edfe99b5e68f0dc53a44b75574a5064c37dc
Iker Irazu <iker@arima.eu>
2015-11-16 19:15:46 +0100

    Integrado Entzunezkoa

--- Euskhazi/www/js/objects.js
+++ Euskhazi/www/js/objects.js
@@ -849,7 +849,7 @@ var pageExams={
 				statementsDivs=statementsDivs+
 				  '<li><a href="#" onClick="loadBerridazketak(&#39;'+level+'&#39;)" id="levels_li_menu_tests_level_'+level+'-proof-'+'berridazketak'+'">'+'Berridazketak'+'</a></li>';
 				statementsDivs=statementsDivs+
-				  '<li><a href="#"  id="levels_li_menu_tests_level_'+level+'-proof-'+'entzumena'+'">'+'Entzumena'+'</a></li>';
+				  '<li><a href="#" onClick="loadEntzunezkoaMenu(&#39;'+level+'&#39;)" id="levels_li_menu_tests_level_'+level+'-proof-'+'entzumena'+'">'+'Entzumena'+'</a></li>';
 				statementsDivs=statementsDivs+
 				  '<li><a href="#" onClick="loadAhozkoa(&#39;'+level+'&#39;)" id="levels_li_menu_tests_level_'+level+'-proof-'+'entzumena'+'">'+'Ahozkoa'+'</a></li>';
 			statementsDivs=statementsDivs+'</ul>';
@@ -1051,6 +1051,100 @@ var pageSinonimoak={
 						'<div class="ui-block-a" style="text-align:left;align-content:center;width:20%;">'+
 						'</div>'+
 						'<div class="ui-block-b" style="text-align:center;align-content:center;width:60%;"><h4 id="correct_answer_sinonimoak" style="margin-bottom:1%;">Erantzun zuzena: 0</h4></div>'+
+						'<div class="ui-block-c" style="text-align:right;align-content:center;width:20%;">'+
+						'</div>'+
+					'</div>'+
+				'</div>';
+			pageDiv.append(headerDiv,contentDiv,footerDiv);
+			return pageDiv;
+		},
+		load: function(i) {
+			
+			alert("load1");
+		}
+			
+};
+var pageEntzunezkoa={
+		
+		createMenu: function(exams,level){
+			if(level==null || exams==null){
+				return null;
+			}
+			$('#page_menu_entzunezkoa').remove();
+			var pageDiv=$('<div data-role="page" id="page_menu_entzunezkoa" name="page_menu_entzunezkoa"></div>');
+			var headerDiv=
+				'<div data-role="header" data-position="fixed" >'+
+					'<h1 style="margin-left:0;margin-right:0;white-space: nowrap;overflow: visible;">'+'Entzunezkoa '+level+'</h1>'+
+				'</div>';
+			
+			var statementsDivs='<ul data-role="listview">';
+			
+			for(var con=0;con<exams.length;con++){
+				statementsDivs=statementsDivs+
+				  '<li><a href="#" onClick="loadExamEntzunezkoa(&#39;'+con+'&#39;,&#39;'+level+'&#39;)" id="menu_exam_entzunezkoa-'+level+'-'+con+'">'+'Azterketa '+(con+1)+'</a></li>';
+			}
+			
+			statementsDivs=statementsDivs+'</ul>';
+			
+			var contentDiv=
+				'<div data-role="content">'+statementsDivs+'</div>';
+			var footerDiv=
+				'<div data-role="footer" data-position="fixed" style="padding-top:1%;">'+
+					'<div class="ui-grid-b">'+
+						'<div class="ui-block-a" style="text-align:left;width:20%;">'+
+						'</div>'+
+						'<div class="ui-block-b" style="text-align:center;width:60%;"><h4 style="margin-bottom:1%;"></h4></div>'+
+						'<div class="ui-block-c" style="text-align:right;width:20%;">'+
+						'</div>'+
+					'</div>'+
+				'</div>';
+			pageDiv.append(headerDiv,contentDiv,footerDiv);
+			return pageDiv;
+		},
+		
+		createExam: function(i,exams,level){
+			if(exams==null || level==null){
+				alert("No se ha podido presentar en pantalla porque se ha pasado un valor menos a uno");
+				return null;
+			}else if(i>=exams.length){
+				alert("No se puede solicitar un numero de examen superior al que se tiene");
+				return null;
+			}
+			$('#page-exam-entzunezkoa').remove();
+			var pageDiv=$('<div id="page-exam-entzunezkoa" data-role="page" ></div>');
+			var headerDiv=
+				'<div data-role="header" data-position="fixed" >'+
+					'<h1 style="margin-left:0;margin-right:0;white-space: nowrap;overflow: visible;">'+'Entzunezkoa '+level+' '+(i+1)+'</h1>'+
+				'</div>';
+			var statementsDivs='<audio controls>'+
+									'<source src="/sdcard/eus.ehu.INTEL901_504021.TTA1516_LS_EX_09v2/audio/Libre.mp3" type="audio/mpeg">'+
+									'Your browser does not support the audio element.'+
+								'</audio>';
+			for(var con=0;con<exams[i].statements.length;con++){
+				statementsDivs+=
+				'<fieldset data-role="controlgroup" data-iconpos="right">'+
+				'<legend id="question-'+con+'">'+(con+1)+'.- '+exams[i].statements[con].statement+'</legend>'+
+				'<input name="radio-choice-entzunezkoa-'+con+'" id="radio-choice-'+con+'a" data-mini="true" value="0" type="radio"/>'+
+				'<label for="radio-choice-'+con+'a" id="label-radio-choice-'+con+'-0">'+exams[i].statements[con].answers[0].first+'</label>'+
+				'<input name="radio-choice-entzunezkoa-'+con+'" id="radio-choice-'+con+'b" data-mini="true" value="1" type="radio"/>'+
+				'<label for="radio-choice-'+con+'b" id="label-radio-choice-'+con+'-1">'+exams[i].statements[con].answers[0].second+'</label>'+
+				'<input name="radio-choice-entzunezkoa-'+con+'" id="radio-choice-'+con+'c" data-mini="true" value="2" type="radio"/>'+
+				'<label for="radio-choice-'+con+'c" id="label-radio-choice-'+con+'-2">'+exams[i].statements[con].answers[0].third+'</label>'+
+				'</fieldset>';
+			}
+			statementsDivs=statementsDivs+'<div id="form-entzunezkoa-buttons">';
+			statementsDivs=statementsDivs+'<a id="correctExams_entzunezkoa" name="onClickCorrectExams" href="" onClick="checkEntzunezkoa(&#39;'+i+'&#39;)" class="ui-btn">Zuzendu</a>'
+			statementsDivs=statementsDivs+'</div>';
+			statementsDivs='<form id="form-entzunezkoa">'+statementsDivs+'</form>';
+			
+			var contentDiv=
+				'<div data-role="content">'+statementsDivs+'</div>';
+			var footerDiv=
+				'<div data-role="footer" data-position="fixed" style="padding-top:1%;">'+
+					'<div class="ui-grid-b">'+
+						'<div class="ui-block-a" style="text-align:left;align-content:center;width:20%;">'+
+						'</div>'+
+						'<div class="ui-block-b" style="text-align:center;align-content:center;width:60%;"><h4 id="correct_answer_entzunezkoa" style="margin-bottom:1%;">Erantzun zuzena: 0</h4></div>'+
 						'<div class="ui-block-c" style="text-align:right;align-content:center;width:20%;">'+
 						'</div>'+
 					'</div>'+
